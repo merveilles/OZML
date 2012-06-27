d3ce0a5e214e451bdebebabb95a0abf51c0004dc
Devine Lu Linvega <me@m0oo.com>
2012-06-27 09:59:27 -0400

    Initial Push

---
+++ Assets/Scripts/Position.js
@@ -0,0 +1,4 @@
+function Update () {
+	print(transform.position.x);
+	
+}
---
+++ Assets/Scripts/Structures.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+// <ozml>
+public class Ozml
+{
+    public OzmlHead Head { get; set; } //<head>
+    public Dictionary<string, OzmlMaterial> Materials { get; set; } //<materials>
+	public Dictionary<string, OzmlObject> Objects; //All objects
+}
+
+// <head>
+public class OzmlHead
+{
+    public string Title { get; set; }			//<title>
+    public OzmlScene Scene { get; set; }	//<scene>
+    public OzmlCamera Camera { get; set; }	//<origin>
+}
+
+// <scene>
+public class OzmlScene
+{
+    public Color Background = Color.white;
+    public FogParameters Fog = new FogParameters { Color = Color.white, Density = 0 };
+    public float Fov = 70;
+}
+
+// fog:
+public struct FogParameters
+{
+    public Color Color;
+    public float Density;
+}
+
+// <origin>
+public class OzmlCamera
+{
+    public Vector3 Position;
+    public Vector3 Rotation;
+	public float Speed = 10;
+}
+
+// .mat
+public class OzmlMaterial
+{
+	public string Name;
+	public Color AmbientDiffuse = Color.white;
+	public Color Specular = Color.white;
+	public Color Emissive = Color.black;
+	public float Power = 16;
+	public float Opacity = 1;
+}
+
+public class OzmlObject
+{
+	public string Id;
+	public Vector3 Position;
+    public Vector3 Rotation; 
+	public Vector3 Size;
+	public string Mat;
+	public string Type;
+}
